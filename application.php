fe1e78f8b708d7be47ec6116d86f937986df760e
tinutreesatom <tinutreesatom@gmail.com>
2018-12-02 21:34:26 +0530

    STUDENT FOLDER

--- application.php
+++ application.php
@@ -1,5 +1,6 @@
 <?php
 $msg='';
+$c=0;
 session_start();
 
 if (!isset($_SESSION['uname'])) 
@@ -10,16 +11,17 @@ if (!isset($_SESSION['uname']))
  else{
  $uname=$_SESSION['uname'];
  include("../connection.php");
-$qry_check="select * from hostel_stud_reg where admn_status='submitted' and ADMNO='".$uname."'";
+$qry_check="SELECT * from hostel_stud_reg where admn_status in('submitted','ranked','allocated') and ADMNO='".$uname."'";
 $res_check=mysqli_query($con,$qry_check);
 $num_rec=mysqli_num_rows($res_check);
 	if($num_rec!=0)
 		{
-			$msg= 'You are already submitted the application...to download the application form <a href="applicationdownload.php"><i>click here</i></a>';
+			//$msg= 'You are already submitted the application...to download the application form <a href="applicationdownload.php"><i>click here</i></a>';
+			$msg= 'You submitted the application...to download the application form <a href="applicationdownload.php"><i>click here</i></a>';
 			
 		}
 		
-$qrys="select * from stud_details sd,class_details cd ,current_class cc where sd.admissionno='".$uname."' and cc.studid='".$uname."' and cd.classid=cc.classid";
+$qrys="SELECT * from stud_details sd,class_details cd ,current_class cc where sd.admissionno='".$uname."' and cc.studid='".$uname."' and cd.classid=cc.classid";
 //print_r($qrys);
 $result=mysqli_query($con,$qrys);
 $records=mysqli_num_rows($result);
@@ -51,16 +53,33 @@ else{
 <html>
 <head >
 
-
+<script type="text/javascript">
+	function validation() {
+		var valid=false;
+		if(document.getElementById('pMob').value=="")
+       			{
+       				alert("Please enter your Contact No.");
+       				valid=true;
+       				return false;
+       			}
+       			if(document.getElementById('distance').value=="")
+       			{
+       				alert("Please enter your distance");
+       				valid=true;
+       				return false;
+       			}
+		
+	}
+</script>
 </head>
 <body id="homebody">
 	<?php
-	include("../header.html");
+	include("studenthome.php");
 		
 	?>
 	<div class="container">
 		
-	<form name="frm1" method ="POST" action="applicationsave.php">
+	<form name="frm1" method ="POST" action="applicationsave.php" onsubmit="return validation()">
 		<div id="rfm"><h2>APPLICATION FORM</h2></div>
 	<h3><?php
 	 if($msg!='')
@@ -94,7 +113,7 @@ else{
 		mob:<input type="text" name="mob_stu" value=<?php echo $mob ?>></td></tr>
 		<tr><td>Address of Parent/Guardian</br>
 			<td><textarea name="txtpaddress" rows="4" cols="35"><?php echo $address ?> </textarea>
-			</br></br>	mob:<input type="text" name="mob_p"></td>
+			</br></br>	mob:<input type="text" name="mob_p" id="pMob" min="10" maxlength="10" required="10 digits should be entered"></td>
 		
 </tr>
 <tr>
@@ -119,7 +138,7 @@ else{
 
 	</select></td>
 	
-	<tr>
+	<!--<tr>
 		<td>Branch</td>
 		<td><select name="brnch">
 		<option value="1">Select one option..</option>
@@ -133,7 +152,7 @@ else{
 		
 	</select></td>
 
-	</tr>
+	</tr>-->
 	
 
 <tr>
@@ -142,26 +161,39 @@ else{
 	</br>Priority II(d)-Lateral Entry students</br>Priority II(e)-First year PG students
 </br>(*priority I student must attach the copy of proof)</td>
 	<td>
-	<table border="1">
+	<table border="2" cellpadding="1" cellspacing="1">
 		<tr>
-		<th>Priorities</th><th>Please tick</th></tr>
+		<th>Priorities</th><th colspan="2">Please tick</th></tr>
 			<tr>
 	<TD>Priority I</TD>
-	<td><input type="checkbox" name="prior[]" value="prior1"></td></tr>
-		<tr><td>Priority II(a)</td><td><input type="checkbox" name="prior[]" value="prior2a"></td></tr>
-	<tr><td>Priority II(d)</td><td><input type="checkbox" name="prior[]" value="prior2d"></td></tr>
-	<tr><td>Priority II(e)</td><td><input type="checkbox" name="prior[]" value="prior2e"></td></tr>
+	<td ><input type="checkbox" name="prior[]" value="prior1" ><script type="text/javascript"> if($(this). prop("checked") == true){
+<?php  $c=1; echo $c;?>
+
+}</script></td>
+
+	<td > SC<input type="checkbox" name="category[]" value="catsc" >
+			ST<input type="checkbox" name="category[]" value="catst">
+			PH<input type="checkbox" name="category[]" value="catph">
+			BPL<input type="checkbox" name="category[]" value="catbpl">
+			<br>Other State<input type="checkbox" name="category[]" value="catoherstate">
+			<br>Central govt. nominee<input type="checkbox" name="category[]" value="catcentral"></TD>
+	</tr>
+		<tr><td>Priority II(a)</td>
+			<td colspan="2"><input type="checkbox" name="prior[]" value="prior2a"></td>
+		</tr>
+	<tr><td>Priority II(d)</td><td colspan="2"><input type="checkbox" name="prior[]" value="prior2d"></td></tr>
+	<tr><td>Priority II(e)</td><td colspan="2"><input type="checkbox" name="prior[]" value="prior2e"></td></tr>
 	
 </table>
 	</td>
 </tr>
 <tr>
 	<td>Family annual Income (Only for BPL category)</br>(*valid income certificate should be attached)</td>
-	<td><input type="text" name="txtincome"></td>
+	<td><input type="text" name="txtincome" id="income"></td>
 </tr>
 <tr>
 	<td>Minimum Google distance from the post</br>office in the address specified in the AAdhaar</br>card to RIT pampady</td>
-	<td><input type="text" name="txtdist"></td>
+	<td><input type="text" name="txtdist" id="distance" required="Plz fill this field"></td>
 </tr>
 <tr>
 </tr>
@@ -170,7 +202,7 @@ else{
 </tr>
 
 <tr align="center" rowspan="5">
-	<br><td  colspan="3" ><input type="submit" name="btsub" value="submit">
+	<br><td  colspan="3" ><input type="submit" name="btsub" value="submit" >
 </td>
 <td colspan="3"><input type="Reset" name="btReset" value="Reset"></td>
 	</tr></br>
--- applicationsave.php
+++ applicationsave.php
@@ -3,6 +3,12 @@ $p0=0;
 $p1=0;
 $p2=0;
 $p3=0;
+$sc=0;
+$st=0;
+$bpl=0;
+$ph=0;
+$o_state=0;
+$central=0;
 session_start();
 
 if (!isset($_SESSION['uname'])) 
@@ -16,10 +22,11 @@ if (!isset($_SESSION['uname']))
  $p_address=$_POST['txtpaddress'];
  $p_mob=$_POST['mob_p'];
  $income=$_POST['txtincome'];
+ $rank=$_POST['txtrank'];
 for($i=0;$i<sizeof($_POST['prior']);$i++)
  {
  	 $priority[$i]=$_POST['prior'][$i];
- 	 echo "<br>".$priority[$i];
+ 	// echo "<br>".$priority[$i];
 
  /*
  if ($priority[$i]=="prior1") {
@@ -77,6 +84,45 @@ if ($p2!=1) {
 if ($p3!=1) {
 	$p3=0;
 }
+//echo 'cat='.sizeof($_POST['category']);
+for($j=0;$j<sizeof($_POST['category']);$j++)
+ {
+ 	 $category[$j]=$_POST['category'][$j];
+ 	 //echo "<br>".$category[$j]."<br>";
+switch($category[$j])
+{
+	case "catsc": $sc=1;break;
+	case "catst": $st=1;break;
+	case "catph": $ph=1;break;
+	case "catbpl": $bpl=1;break;
+	case "catoherstate": $o_state=1;break;
+	case "catcentral": $central=1;break;
+	default:$sc=0;$st=0;$ph=0;$bpl=0;$o_state=0;$central=0;
+}
+}
+if ($sc!=1) {
+	$sc=0;
+}
+if ($st!=1) {
+	$st=0;
+}
+if ($ph!=1) {
+	$ph=0;
+}
+if ($o_state!=1) {
+	$o_state=0;
+}
+if ($bpl!=1) {
+	$bpl=0;
+}
+if ($central!=1) {
+	$central=0;
+}
+
+/*echo "<br>sc=".$sc."<br>".$st."<br>";
+echo "<br>central=".$central."<br> ph=".$ph."<br>";
+echo "<br>bpl=".$bpl;*/
+
 
 include("../connection.php");
 
@@ -88,19 +134,19 @@ while ( $row= mysqli_fetch_assoc($dobres)) {
 }
 $qry="insert into hostel_login(username,password,usertype)values('".$uname."','".$dob."','student')";
 $res=mysqli_query($con,$qry);
-$qryin="insert into hostel_stud_reg(admno,admn_status,distance,income,parent_address,parent_mob,priority1,priority2a,priority2d,priority2e)values('".$uname."','submitted','".$dist."','".$income."','".$p_address."','".$p_mob."','".$p0."','".$p1."','".$p2."','".$p3."')";
+$qryin="insert into hostel_stud_reg(admno,admn_status,distance,income,parent_address,parent_mob,priority1,priority2a,priority2d,priority2e,Entrance_rank,sc,st,ph,bpl,other_state,central)values('".$uname."','submitted','".$dist."','".$income."','".$p_address."','".$p_mob."','".$p0."','".$p1."','".$p2."','".$p3."','".$rank."','".$sc."','".$st."','".$ph."','".$bpl."','".$o_state."','".$central."')";
 //print_r($qryin);
 $result=mysqli_query($con,$qryin);
 if ($result)
 {
 	mysqli_close($con);
 	?>
-	<a href="ranklistgenerate.php"></a>
+	
 	<?php 
-	echo '<script type="text/javascript">alert("Succefully registered");window.location=\'application.php\';</script>';
+	//echo '<script type="text/javascript">alert("Succefully registered");window.location=\'application.php\';</script>';
 	//echo "alert('Succefully registered')";
 	
-	//echo '<script type="text/javascript">alert("Succefully registered");window.location=\'application.php\';</script>';
+	echo '<script type="text/javascript">alert("Succefully registered");window.location=\'application.php\';</script>';
 
 	
 	//echo 'Redirect("registration.html")'.'alert("Succefully registered")';?>
@@ -111,7 +157,8 @@ else
 	{
 		//need to change the location of the web page when registration failed to......echo '<script type="text/javascript">alert("Registration failed");window.location=\'application.php\';</script>';
 	//echo '<script type="text/javascript">alert("Registration failed");window.location=\'applicationsave.php\';</script>';
-		echo "Registration failed";
+		echo '<script type="text/javascript">alert("Registration failed");window.location=\'application.php\';</script>';
+
 	
 }
 
